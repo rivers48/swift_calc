caa321a7462a55fd75f746b5f9b8010fa14c5107
rivers48 <dialog320@gmail.com>
2017-06-05 18:56:35 +0300

    Add files via upload

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by rivers on 11.04.17.
+//  Copyright © 2017 rivers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOftyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOftyping {
+            let textCurrentlyOnTheDisplay = display.text!
+            display.text = textCurrentlyOnTheDisplay + digit
+    } else {
+            display.text = digit
+            userIsInTheMiddleOftyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+
+    private var brain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOftyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOftyping =  false
+        }
+        if let mathematycalSymbol = sender.currentTitle {
+            brain.performOperation(mathematycalSymbol)
+        }
+        
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
+    
+}
